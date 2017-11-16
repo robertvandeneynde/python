e03d481c94c0f5ec8244387c2057e2e31be33127
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2017-11-16 19:04:19 +0100

    auto

--- annotate.py
+++ annotate.py
@@ -19,6 +19,7 @@ p.add_argument('-o', '--out', default='annotate.csv')
 p.add_argument('--append', action='store_true', help='Append entries to csv')
 p.add_argument('--header', action='store_true', help='Insert header with date')
 p.add_argument('--ignore-empty', action='store_true', help='Do not output empty values')
+# p.add_argument('--check', action='store_true', help='Do not write anything, just check current annotations')
 
 p.add_argument('--boolean', action='store_true', help='Ask True (with keyboard J) or False (with keyboard F)')
 p.add_argument('--true', action='store_true', help='What to write when True (J) (Right)', default='True')
@@ -27,12 +28,16 @@ p.add_argument('--false', action='store_true', help='What to write when False (F
 p.add_argument('--width', type=int, default=800)
 p.add_argument('--height', type=int, default=800)
 p.add_argument('--rotate', type=int, choices=[0,90,180,270,-90,-180,-270], default=0, help='rotate all images by x degrees')
+p.add_argument('--pos-x', type=int, default=0)
+p.add_argument('--pos-y', type=int, default=0)
+p.add_argument('--zoom', type=Fraction, default=1)
 
 g = p.add_mutually_exclusive_group()
-g.add_argument('--from-file', help='Take list of files from first column in csv file (in dir)')
-g.add_argument('--ignore-file', help='Take list of files that are NOT in first column in csv file (in dir)')
+g.add_argument('--from-file', help='Take list of files from first column in from-file csv file (in dir)')
+g.add_argument('--ignore-file', help='Take list of files that are NOT in first column in ignore-file csv file (in dir)')
 
 p.add_argument('--continue', action='store_true', help='Continue previous work (ignore files in annotate.csv and append to it)')
+p.add_argument('--new-column', action='store_true', help='Add new column in annotate.csv instead of creating new file. Order will be the one of annotate.csv, then normal order for the ones not present')
 
 a = args = p.parse_args()
 
@@ -40,10 +45,21 @@ if getattr(a, 'continue'):
     a.ignore_file = a.out
     a.append = True
 
+if args.from_file:
+    assert args.from_file.endswith('.csv'), "must be csv"
+if args.ignore_file:
+    assert args.ignore_file.endswith('.csv'), "must be csv"
+
+if args.new_column:
+    raise ValueError('not implemented')
+
 def key_natural_sort(filename):
     """
-    'File 28 Page 2' → ['File ', 28, ' Page ', 2]
-    sorted(['1.png', '2.png', '3.png', '10.png', '20.png'], key=key_natural_sort) == ['1.png', '2.png', '3.png', '10.png', '20.png']
+    >>> key_natural_sort('File 28 Page 2')
+    ['File ', 28, ' Page ', 2]
+    
+    >>> sorted(['1.png', '2.png', '3.png', '10.png', '20.png'], key=key_natural_sort)
+    ['1.png', '2.png', '3.png', '10.png', '20.png']
     """
     return [
         x if x else int(y)
@@ -53,28 +69,33 @@ def key_natural_sort(filename):
 def read_file_names(filename):
     ''' return first column of file <filename> '''
     with open(filename) as f:
-        reader = csv.reader(f, delimiter=';')
-        L = []
-        for data in reader:
-            if data:
-                L.append(data[0])
-        return L
+        return [data[0] for data in csv.reader(f, dialect='excel') if data]
 
 if args.from_file:
     file_names = read_file_names(args.from_file)
 else:
     file_names = os.listdir(args.dir)
     
-    if args.ignore_file:
-        if not os.path.exists(args.ignore_file):
-            print('Warning, file ', args.ignore_file, 'does not exist to read ignore list')
-        else:
-            ignore_list = read_file_names(args.ignore_file)
-            for l in ignore_list:
-                try:
-                    file_names.remove(l)
-                except:
-                    print('Warning, file ', l, 'does not exist to ignore')
+if args.ignore_file:
+    if not os.path.exists(args.ignore_file):
+        print('Warning, file ', args.ignore_file, 'does not exist to read ignore list')
+    else:
+        ignore_list = read_file_names(args.ignore_file)
+        print('- INFO Ignored {}: {}'.format(len(ignore_list), ', '.join(ignore_list[:5]) + '...' * (len(ignore_list) > 5)))
+        for l in ignore_list:
+            try:
+                file_names.remove(l)
+            except:
+                print('Warning, file ', l, 'does not exist to ignore')
+
+if args.new_column and os.path.exists(args.out):
+    ignore_list = read_file_names(args.out)
+    for l in ignore_list:
+        try:
+            file_names.remove(l)
+        except:
+            print('Warning, file ', l, 'does not exist to ignore')
+    ... # TODO: new_column
 
 FileInfo = namedtuple('FileInfo', ('path', 'name'))
 
@@ -93,7 +114,7 @@ else:
     if os.path.exists(args.out):
         os.rename(args.out, args.out + datetime.now().strftime('.%Y-%m-%dT%Hh%S~'))
     opened_csv = open(args.out, 'w')
-opened_csv_writer = csv.writer(opened_csv, delimiter=';')
+opened_csv_writer = csv.writer(opened_csv, dialect='excel')
 
 if args.header:
     opened_csv_writer.writerow([
@@ -113,7 +134,7 @@ def to_p1p2(R:'x,y,w,h'):
 def refresh_image():
     global image, photo
     image = (image_base
-        .rotate(args.rotate)
+        .rotate(rotate_angle)
         .resize(tuple(map(int, (image_base.width * zoom_factor, image_base.height * zoom_factor) )))
         .crop( to_p1p2((int(pos_image[0] * zoom_factor), int(pos_image[1] * zoom_factor), WIDTH,HEIGHT)) )
     )
@@ -121,12 +142,10 @@ def refresh_image():
     label.config(image=photo)
 
 def onMouseDown(event):
-    #print('D', event.x, event.y)
     global drag_point
     drag_point = event.x, event.y
 
 def onMouseDownMove(event):
-    #print('M', event.x, event.y)
     global image, photo, pos_image, drag_point
     
     dx = int((event.x - drag_point[0]) / zoom_factor)
@@ -138,19 +157,15 @@ def onMouseDownMove(event):
     refresh_image()
 
 def onMouseUp(event):
-    #print('U', event.x, event.y)
+    print('--pos-x={} --pos-y={}'.format(*pos_image))
     global drag_point
     drag_point = None
 
 def onScroll(event):
-    global zoom_factor, photo, image
     if event.num == 4 or event.delta > 0:
-        zoom_factor *= Fraction('1.1')
+        onZoom(1)
     if event.num == 5 or event.delta < 0:
-        zoom_factor /= Fraction('1.1')
-    #print('Zoom', float(zoom_factor))
-    
-    refresh_image()
+        onZoom(-1)
     
 def displayprop(x, L):
     pprint([getattr(x, l) for l in L])
@@ -181,6 +196,7 @@ def changeImage(direction, boolean=None):
     else:
         file_index = max(0, file_index + direction)
         image_base = Image.open(files[file_index].path)
+        infovariable.set("{} ({}/{})".format(files[file_index].name, 1+file_index, len(files)))
         refresh_image()
     
     if not args.boolean:
@@ -206,24 +222,62 @@ def onKeyDown(event):
     # if event.state == 20 and event.keysym == 'Left':
     #     changeImage(-1)
 
-file_index = 0
-image_base = Image.open(files[file_index].path)
+def onRotate(direction):
+    global rotate_angle
+    
+    rotate_angle += direction * 90
+    if abs(rotate_angle) >= 360:
+        rotate_angle %= 360
+    
+    print('--rotate={}'.format(rotate_angle))
+    
+    refresh_image()
+
+def onZoom(direction):
+    global zoom_factor
+    
+    zoom_factor *= Fraction('12/10') ** direction # (f *= 1.1) if d = +1 else (f /= 1.1) if d = -1 else (f *= 1.1 * 1.1) if d = +2 etc.
+    
+    print('--zoom={}'.format(zoom_factor))
+    
+    refresh_image()
+
+# BEGIN GUI
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.pack() # fill=X, expand=True) # expand=True) # sticky=(N, W, E, S), expand=True)
+mainframe.pack(fill=BOTH, expand=True) # fill=X, expand=True) # expand=True) # sticky=(N, W, E, S), expand=True)
 # mainframe.columnconfigure(0, weight=1)
 # mainframe.rowconfigure(0, weight=1)
 
-if not args.boolean:
+infovariable = StringVar()
+infovariable_label = ttk.Label(mainframe, textvariable=infovariable, anchor=S)
+infovariable_label.pack(fill=X, side=TOP)
+infovariable_label.bind('<Button-1>', lambda x: print(infovariable.get()))
+
+# ttk.Button(mainframe, text='Zoom -', command=lambda: onZoom(1)).pack(side=BOTTOM)
+ttk.Button(mainframe, text='Rotate', command=lambda: onRotate(+1)).pack(side=BOTTOM)
+# ttk.Button(mainframe, text='Zoom +', command=lambda: onZoom(-1)).pack(side=BOTTOM)
+
+if args.boolean:
+    widget = ttk.Entry(mainframe, justify=CENTER)
+    widget.insert(0, '(F) False <-> True (J)')
+    widget.config(state=DISABLED)
+else:
     text_variable = StringVar()
 
-    text_variable_entry = ttk.Entry(mainframe, justify=CENTER, textvariable=text_variable)
-    text_variable_entry.pack(side=BOTTOM, fill=X) # (column=0, row=1, sticky=(W, E))
-else:
-    ttk.Label(text='(F) False <-> True (J)').pack(side=BOTTOM, fill=X) # (column=0, row=1, sticky=(W, E))
+    widget = text_variable_entry = ttk.Entry(mainframe, justify=CENTER, textvariable=text_variable)
 
-pos_image = 0,0
-zoom_factor = Fraction(1)
+widget.pack(side=BOTTOM, anchor=S) # (column=0, row=1, sticky=(W, E))
+
+# END GUI
+
+file_index = 0
+image_base = Image.open(files[file_index].path)
+infovariable.set("{} ({}/{})".format(files[file_index].name, 1+file_index, len(files)))
+
+pos_image = args.pos_x, args.pos_y
+zoom_factor = args.zoom
+rotate_angle = args.rotate
 drag_point = None
 
 label = ttk.Label(mainframe) # image=photo
@@ -235,6 +289,7 @@ label.bind('<MouseWheel>', onScroll)
 label.bind('<Button-4>', onScroll)
 label.bind('<Button-5>', onScroll)
 label.bind('<Button-1>', onMouseDown)
+label.bind('<Double-Button-1>', lambda x:onRotate(+1))
 label.bind('<B1-Motion>', onMouseDownMove)
 label.bind('<ButtonRelease-1>', onMouseUp)
 # On Windows, you bind to <MouseWheel> and you need to divide event.delta by 120 (or some other factor depending on how fast you want the scroll)
