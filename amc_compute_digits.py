1ef16aa7c9145a08c91e5430b8177f801c15aa48
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2017-12-22 19:24:26 +0100

    more generality

--- amc_compute_digits.py
+++ amc_compute_digits.py
@@ -6,6 +6,8 @@ assert sys.version_info[0] >= 3, "Python 3 please"
 
 FILENAME = 'generateurAMC_B.csv' 
 
+POLICIES = {'EmptyColumnMeansZero'} # set() # {'NoSignMeansPlus'}
+
 assert FILENAME.endswith('.csv')
 
 with open(FILENAME) as f:
@@ -20,30 +22,67 @@ FIELDS = set(csvMatrix[0].keys()) # DictReader enforces that all(l.keys() == FIE
 assert MANDATORY_COLUMNS <= FIELDS, "Missing columns: {}".format(MANDATORY_COLUMNS - FIELDS)
 Questions = {f for f in FIELDS - MANDATORY_COLUMNS if not f.startswith('TICKED:')}
 
-def parseQF(csvLine:list, name:'QF1b'):
-    def parsePart(part:'digits|exp', expectedNumbers:3):
+class EmptyColumn(Exception):
+    pass
+
+class MultipleTicksInColumn(Exception):
+    pass
+
+def parseQF(csvLine:list, name:'QF1b', *, policies=POLICIES):
+    def parsePart(part:'digits|exp', expectedNumbers:3, *, base=10, hasSign=True, direction:'plus|minus'='plus'):
+        """
+        Parse a signed number.
+        if hasSign, the first two boxes contains the sign (first = +, second = -).
+        Then we have base=10 expectedNumbers boxes representing numbers. For example expectedNumbers = 3, digits = [6,0,9].
+        If direction == "plus" Then Digits represents 609
+        If direction == "minus" Then Digits represents 6.09
+        """
+        assert part in ('digits', 'exp') and direction in ('plus', 'minus')
     
         field = 'TICKED:{}{}'.format(name, part)
-        Mantiss = list(map(int, csvLine[field.format(name)].split(';')))
+        Ticks = list(map(int, csvLine[field.format(name)].split(';')))
         
-        assert len(Mantiss) == 2 + expectedNumbers * 10
+        # Example: Ticks = [0,1, 0,0,0,1,0,0,0,0,0,0, 0,1,0,0,0,0,0,0,0,0]
         
-        Sign = Mantiss[0:2]
-        Digits = [Mantiss[2 + n : 2 + n + 10] for n in range(0, 10 * expectedNumbers, 10)]
+        assert len(Ticks) == (2 if hasSign else 0) + expectedNumbers * base
+        
+        if hasSign:
+            Sign = Ticks[0:2]
+            Digits = [Ticks[2+n : 2+n+base] for n in range(0, base * expectedNumbers, base)]
+        else:
+            Sign = [1,0] # Positive
+            Digits = [Ticks[n : n+base] for n in range(0, base * expectedNumbers, base)]
+        
+        if Sign == [0,0] and 'NoSignMeansPlus' in policies:
+            Sign = [1,0]
+        
+        # Example if hasSign == True and base == 10: Sign = [0,1]; Digits = [[0,0,0,1,0,0,0,0,0,0], [0,1,0,0,0,0,0,0,0,0]]
         
         for col in [Sign] + Digits:
-            assert sum(col) == 1, "User ticked two columns in a column (for {})".format(part)
+            if sum(col) == 0:
+                if 'EmptyColumnMeansZero' in policies:
+                    col[0] = 1
+                else:
+                    raise EmptyColumn("User ticked zero columns in a column (for {})".format(part))
+            if sum(col) > 1:
+                raise MultipleTicksInColumn("User ticked two columns in a column (for {})".format(part))
         
-        sign = (+1 if Sign[0] else -1)
-        digits = [next(i for i in range(len(D)) if D[i]) for D in Digits]
+        sign = +1 if Sign[0] else -1
+        digits = [D.index(1) for D in Digits]
         
-        number = sign * sum(n * 10 ** (-i) for i,n in enumerate(digits))
+        # Example: sign = -1; digits = [3, 1]
+        
+        number = (sign * sum(n * base ** (-i) for i,n in enumerate(digits)) if direction == 'minus' else
+                  sign * sum(n * base ** i for i,n in enumerate(reversed(digits))) if direction == 'plus' else None)
     
+        # Example if direction == 'minus': number = -3.1
+        # Example if direction == 'plus':  number = -31
+        
         return number
     
-    number = parsePart('digits', 3)
-    numberExp = parsePart('exp', 1)
-    numberFinal = number * 10 ** numberExp
+    number = parsePart('digits', 3, hasSign=True, direction='minus')
+    numberExp = parsePart('exp', 1, hasSign=True, direction='plus')
+    numberFinal = number * base ** numberExp
     
     return numberFinal
 
