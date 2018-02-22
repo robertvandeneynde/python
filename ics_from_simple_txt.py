e9c90a300d0078695fe9d71cde69e667b1706eb0
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2018-02-22 03:17:37 +0100

    Ics creator

---
+++ ics_from_simple_txt.py
@@ -0,0 +1,173 @@
+# coding: utf-8
+
+import re
+from re import compile as Re
+import pytz
+from datetime import datetime, date, time, timedelta
+from itertools import filterfalse
+from collections import defaultdict
+
+IN_FILE = 'physique-ba2.txt'
+ASSISTANTS = 'watchi decrol mahrou tillema gernier dierick arnhem jvc vanden friob'.split()
+PREFIX_PER_ASSISTANT = {}
+DEFAULT_PREFIX = "TP"
+ALL_DURATION = '6h'
+SEANCES = 'num' # empty meaning not present, 'num' meaning numbers, 'letters' meaning letters, 'mixed' meaning numbers and text
+GROUPS_PREFIXES = () # groups begin with a prefix, then a number
+
+if SEANCES:
+    SEANCES = {'letter': 'letters'}.get(SEANCES, SEANCES)
+    assert SEANCES in ('num', 'letters', 'mixed')
+
+GROUPS = GROUPS_PREFIXES
+
+DAYS = 'lun|mar|mer|jeu|ven|sam|dim'.split('|')
+DAY = Re('|'.join(DAYS))
+
+HOUR = Re('(\d+)[h:](\d*)')
+HOUR_FROM = Re('(de|from)(\d+)[h:](\d*)')
+HOUR_TO = Re('(à|to)(\d+)[h:](\d*)')
+DATE = Re('(\d+)/(\d+)')
+WEEK_SHIFT = Re('([+-]\d*)w')
+
+GROUP = Re('(' + '|'.join(map(re.escape, map(str, GROUPS))) + ')(\d*)', re.I)
+SEANCE = Re('S({})'.format({'num': '\d+', 'letters':'\w+', 'mixed':'[\d\w]+'}[SEANCES]), re.I) if SEANCES else Re('')
+ASSISTANT = Re('|'.join(map(re.escape, ASSISTANTS)), re.I)
+
+DURATION_RE = Re('(\d+)h(\d\d)?(min)?|(\d+)min')
+assert DURATION_RE.fullmatch(ALL_DURATION), '{ALL_DURATION} is not like "6h" or "6h30" or "5min"'
+
+def convert_duration(string):
+    hour, minute, _, single_minute = DURATION_RE.fullmatch(string).groups()
+    if single_minute:
+        return timedelta(minutes=int(single_minute))
+    else:
+        return timedelta(hours=int(hour), minutes=int(minute or 0))
+
+DURATION = convert_duration(ALL_DURATION)
+
+RE_LIST = (
+    [DAY, HOUR, HOUR_FROM, HOUR_TO, DATE, WEEK_SHIFT]
+    + [ASSISTANT] * bool(ASSISTANT)
+    + [GROUP] * bool(GROUPS)
+    + [SEANCE] * bool(SEANCES)
+)
+
+is_none = lambda x: x is None
+is_not_none = lambda x: x is not None
+
+# [convert(L[0][i]) for i in range(len(L[0]))]
+
+def naive_to_utc(naive):
+    return pytz.timezone('Europe/Brussels').localize(naive, is_dst=None).astimezone(pytz.utc)
+
+def convert(bit):
+    M = [(r, r.fullmatch(bit)) for r in RE_LIST]
+    F = [(r,m) for r,m in M if m]
+    assert len(F) == 1, f"in {bit}: exactly one must match, but {len(F)} matched"
+    return F[0]
+
+text_bits_per_assistant = defaultdict(list)
+
+with open(IN_FILE) as f:
+    lines = [l.strip('\n') for l in f]
+
+for line in filterfalse(Re('\s*#').match, filter(bool, lines)):
+    
+    year = 2018
+    delta = timedelta(0)
+    month, day, weekday, hour, minute, hour_end, minute_end, *nones = [None] * 100
+
+    assistants = []
+    groups = []
+    seances = []
+    
+    for r, m in map(convert, line.split()):
+        if r is DAY:
+            DAYS.index(m.group(0))
+        elif r is HOUR:
+            hour, minute = int(m.group(1)), int(m.group(2) or 0)
+        elif r is HOUR_FROM:
+            hour, minute = int(m.group(2)), int(m.group(3) or 0)
+        elif r is HOUR_TO:
+            hour_end, minute_end = int(m.group(2)), int(m.group(3) or 0)
+        elif r is DATE:
+            day, month = int(m.group(1)), int(m.group(2))
+        elif r is GROUP:
+            groups.append((m.group(1), int(m.group(2) or 1)))
+        elif r is SEANCE:
+            seances.append(int(m.group(1)))
+        elif r is ASSISTANT:
+            assistants.append(m.group(0).lower())
+        elif r is WEEK_SHIFT:
+            delta = timedelta(weeks=int(m.group(1)))
+    
+    if not ASSISTANTS:
+        assert not assistants
+        assistants = ['default']
+    
+    assert len(assistants) > 0, f"on line {line}: at least one assistant"
+    
+    if GROUPS:
+        assert len(assistants) == len(groups), f"on line {line}: length differs, assistants:{len(assistants)}, groups:{len(groups)}, seances:{len(seances)}"
+    assert len(assistants) == len(seances), f"on line {line}: length differs, assistants:{len(assistants)}, groups:{len(groups)}, seances:{len(seances)}"
+    assert all(map(is_not_none, (month, day, hour, minute))), f"on line {line}: missing required parts in {line}: month {month}, day {day}, hour {hour}, minute {minute}"
+
+    base_time = datetime(year, month, day, hour, minute) + delta
+    
+    if hour_end is not None:
+        assert DURATION == datetime.combine(base_time, time(hour_end, minute_end)) - base_time, "Duration must be {ALL_DURATION}"
+    
+    if weekday is not None:
+        assert base_time.weekday() == weekday
+        
+    utc = naive_to_utc(base_time)
+    
+    beg = utc
+    end = utc + DURATION
+    
+    for i in range(len(assistants)):
+        assistant = assistants[i]
+        seance = seances[i]
+        
+        prefix = PREFIX_PER_ASSISTANT.get(assistant, DEFAULT_PREFIX)
+        
+        if GROUPS:
+            grouptype, groupnum = groups[i]
+            summary = f'{prefix} {grouptype}{groupnum} S{seance}'
+        else:
+            summary = f'{prefix} S{seance}'
+        
+        
+        text_bits_per_assistant[assistant].append(f"""
+            BEGIN:VEVENT
+            DTSTART:{beg:%Y%m%dT%H%M%S}Z
+            DTEND:{end:%Y%m%dT%H%M%S}Z
+            LOCATION:Gehol
+            SUMMARY:{summary}
+            END:VEVENT
+        """)
+
+def remove_beginning_whitespace(line_string):
+    return re.sub('^\s+', '', line_string)
+
+for assistant, text_bits in text_bits_per_assistant.items():
+  with open(f'{assistant}.ics', 'w') as outfile:
+    text_bits.insert(0, '''
+        BEGIN:VCALENDAR
+        PRODID:-//Google Inc//Google Calendar 70.9054//EN
+        VERSION:2.0
+        CALSCALE:GREGORIAN
+        METHOD:PUBLISH
+    ''') # X-WR-CALNAME:TP Robert
+    
+    text_bits.append('''
+        END:VCALENDAR
+    ''')
+    
+    outfile.write('\n'.join(
+        remove_beginning_whitespace(line_string)
+        for line_string in
+        '\n'.join(text_bits).split('\n')
+        if line_string.strip()
+    ) + '\n')
